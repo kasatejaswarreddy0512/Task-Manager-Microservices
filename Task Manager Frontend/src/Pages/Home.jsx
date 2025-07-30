d19ec7bfddd8a6ffe6211c661b0fcac6a6e4141c
kasatejaswarreddy0512 <ktrreddy64@gmail.com>
2025-07-30 18:21:02 +0530

    sidebar commit

--- Task Manager Frontend/src/Pages/Home.jsx
+++ Task Manager Frontend/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import SideBar from "./SideBar";
 const Home = () => {
   return (
     <div className="lg:flex px-5 lg:px-20 pt-[2.9vh">
-      <div className="hidden lg:block w-[25vh] relative">
+      <div className="hidden lg:block w-[25vh] relative h-[85vh]">
         <SideBar />
       </div>
       <div className="right-side-part"></div>
--- Task Manager Frontend/src/Pages/SideBar.jsx
+++ Task Manager Frontend/src/Pages/SideBar.jsx
@@ -1,46 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
-import { Avatar } from "@mui/material";
+import { Avatar, Button } from "@mui/material";
 
 const SideBar = () => {
   const menu = [
-    { name: "Home", value: "Home", role: ["ADMIN", "USER"] },
+    { name: "HOME", value: "Home", role: ["ADMIN", "USER"] },
     { name: "DONE", value: "DONE", role: ["ADMIN", "USER"] },
-    { name: "Assigned", value: "Assigned", role: ["ADMIN"] },
-    { name: "Not Assigned", value: "Not Assigned", role: ["ADMIN"] },
-    { name: "Create New task", value: "Create New task", role: ["ADMIN"] },
-    { name: "Notification", value: "Notification", role: ["USER"] },
-    { name: "Log Out", value: "Log Out", role: ["ADMIN", "USER"] },
+    { name: "ASSIGNED", value: "ASSIGNED", role: ["ADMIN"] },
+    { name: "NOT ASSIGNED", value: "NOT ASSIGNED", role: ["ADMIN"] },
+    { name: "CREATE NEW TASK", value: "CREATE NEW TASK", role: ["ADMIN"] },
+    { name: "NOTIFICATION", value: "NOTIFICATION", role: ["USER"] },
   ];
+  const role = "ADMIN";
+
+  const [activeMenu, setActiveMenu] = useState("HOME");
 
   return (
-    <div className="card min-h-[85vh] flex flex-col justify-center fixed w-[20vw]">
-      <div className="space-y-4 h-full">
-        <Avatar
-          sx={{ backgroundColor: "#c24dd0", width: "8rem", height: "8rem" }}
-          className="border-2 border-red-500 mx-auto"
+    <div className="card min-h-[85vh] w-[20vw] flex flex-col justify-center items-center">
+      {/* Avatar */}
+      <Avatar
+        sx={{ backgroundColor: "#c24dd0", width: "8rem", height: "8rem" }}
+        className="border-2 border-red-500 mb-6"
+      >
+        T
+      </Avatar>
+
+      {/* Menu Items */}
+      <div className="flex flex-col w-full space-y-2 px-4">
+        {menu
+          .filter((item) => item.role.includes(role))
+          .map((item, index) => (
+            <p
+              onClick={() => setActiveMenu(item.name)}
+              key={index}
+              className={`text-sm text-center text-white mt-2 border border-gray-300 py-2 rounded-full  cursor-pointer ${
+                activeMenu == item.name ? "activeMenuItem" : "menuItem"
+              }`}
+            >
+              {item.name}
+            </p>
+          ))}
+        <Button
+          onClick={() => console.log("Log Out Successfully")}
+          sx={{ padding: "6px", borderRadius: "60px", marginTop: "6px" }}
+          className="logOut"
         >
-          T
-        </Avatar>
+          Log Out
+        </Button>
       </div>
-      <button className=" btn bg-[#c24dd0] text-white p-2 w-full mt-5">
-        Home
-      </button>
-      <button className="btn bg-[#c24dd0] text-white p-2 w-full mt-5">
-        DONE
-      </button>
-      <button className="btn bg-[#c24dd0] text-white p-2 w-full mt-5">
-        Not Assigned
-      </button>
-      <button className="btn bg-[#c24dd0] text-white p-2 w-full mt-5">
-        In Progress
-      </button>
-      <button className="btn bg-[#c24dd0] text-white p-2 w-full mt-5">
-        Completed
-      </button>
-      <button className="btn bg-[#c24dd0] text-white p-2 w-full mt-5">
-        Log Out
-      </button>
     </div>
   );
 };
